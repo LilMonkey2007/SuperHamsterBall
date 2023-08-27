30f559fef7bca783aeb99234294205d667b46cbd
shuxien97 <131791544+shuxien97@users.noreply.github.com>
2023-08-28 03:46:29 +0800

    updated

--- Assets/Script/DisplayScore.cs
+++ Assets/Script/DisplayScore.cs
@@ -5,21 +5,13 @@ using TMPro;
 
 public class DisplayScore : MonoBehaviour
 {
-<<<<<<< Updated upstream
-=======
+
     [SerializeField]
     private FloatSO ScoreSO;
->>>>>>> Stashed changes
 
     [SerializeField]
     public TMP_Text scoreText;
 
-<<<<<<< Updated upstream
-    [SerializeField]
-    private FloatSO ScoreSO;
-
-=======
->>>>>>> Stashed changes
     private void Start()
     {
         scoreText.text = ScoreSO.Value.ToString();
