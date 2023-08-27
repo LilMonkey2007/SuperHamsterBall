12fffc3bf8058b01c2a558d15ea8f095103a54af
shuxien97 <131791544+shuxien97@users.noreply.github.com>
2023-08-28 00:54:26 +0800

    test

---
+++ Assets/Script/Leaderboard.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField]
+    private List<TextMeshProUGUI> names;
+    [SerializeField]
+    private List<TextMeshProUGUI> scores;
+
+    private string publicLeaderboardKey =
+        "84fffec34a45a6efec9ca0f02b8d7beb5c23cb7f8b944d99fd09e9f705764ef8";
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) =>
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                names[i].text = msg[i].Username;
+                names[i].text = msg[i].Score.ToString();
+            }
+        }));
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username,
+            score, ((msg) =>
+            {
+                GetLeaderboard();
+            }));
+    }
+}
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField inputName;
+
+    public UnityEvent<string, int> submitScoreEvent;
+
+    [SerializeField]
+    private FloatSO ScoreSO;
+
+    public void SubmitScore()
+    {
+        submitScoreEvent.Invoke(inputName.text,int.Parse(ScoreSO.Value));
+    }
+}
